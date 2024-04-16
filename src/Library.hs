6058105f85c0168cdd2e7ad11efa33b3198539b0
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-04-16 10:56:41 -0300

    Modelado de informacion

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,30 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+data Banda = UnaBanda {
+    nombre :: String,
+    repertorio :: [Cancion]
+}
+
+data Cancion = UnaCancion{
+    titulo :: String,
+    duracion :: Number,
+    instrumentosUsados :: [Instrumentos]
+}
+
+data Instrumentos = Guitarra | Bateria | Teclado | Bajo 
+
+--Instanciamos las canciones ya existentes
+patternMatching :: Cancion
+patternMatching = UnaCancion{titulo = "Pattern Matching", duracion = 4, instrumentosUsados [Guitarra, Bajo, Bateria]}
+
+seisDieciocho :: Cancion
+seisDieciocho = UnaCancion{titulo = "Seis Dieciocho", duracion = 3, instrumentosUsados [Teclado, Guitarra]}
+
+laVidaEnHaskell :: Cancion
+laVidaEnHaskell = UnaCancion{titulo = "La vida en Haskell", duracion = 5, instrumentosUsados []}
+
+
+pdePop :: Banda
+pdePop = UnaBanda{nombre = "PdePop", repertorio = [patternMatching]}
