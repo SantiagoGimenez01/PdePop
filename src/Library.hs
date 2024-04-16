0fdd78d54a0965bb63b39893b3868eb6831caa92
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-04-16 12:02:36 -0300

    Funcion aceptacion implementada y probada

--- src/Library.hs
+++ src/Library.hs
@@ -17,16 +17,26 @@ data Cancion = UnaCancion{
 
 data Instrumentos = Guitarra | Bateria | Teclado | Bajo 
 
+--Instanciamos la banda
+pdePop :: Banda
+pdePop = UnaBanda{nombre = "PdePop", repertorio = [patternMatching, seisDieciocho, laVidaEnHaskell]}
+
 --Instanciamos las canciones ya existentes
 patternMatching :: Cancion
-patternMatching = UnaCancion{titulo = "Pattern Matching", duracion = 4, instrumentosUsados [Guitarra, Bajo, Bateria]}
+patternMatching = UnaCancion{titulo = "Pattern Matching", duracion = 4, instrumentosUsados = [Guitarra, Bajo, Bateria]}
 
 seisDieciocho :: Cancion
-seisDieciocho = UnaCancion{titulo = "Seis Dieciocho", duracion = 3, instrumentosUsados [Teclado, Guitarra]}
+seisDieciocho = UnaCancion{titulo = "Seis Dieciocho", duracion = 3, instrumentosUsados = [Teclado, Guitarra]}
 
 laVidaEnHaskell :: Cancion
-laVidaEnHaskell = UnaCancion{titulo = "La vida en Haskell", duracion = 5, instrumentosUsados []}
+laVidaEnHaskell = UnaCancion{titulo = "La vida en Haskell", duracion = 5, instrumentosUsados = []}
+
+indiceDeAceptacion :: Cancion -> Number
+indiceDeAceptacion cancion
+    |head (titulo cancion) == 'M' = 500
+    |even (duracion cancion) = length (titulo cancion) * 10
+    |null(instrumentosUsados cancion) = 10
+    |otherwise = 0
 
 
-pdePop :: Banda
-pdePop = UnaBanda{nombre = "PdePop", repertorio = [patternMatching]}
+
